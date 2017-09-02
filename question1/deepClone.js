432747f52816180b916d7324ea0c8c46aadf976f
Bruce Graham <brucegraham11@gmail.com>
2017-09-02 16:35:25 -0700

    Add Question 1 files

---
+++ question1/deepClone.js
@@ -0,0 +1,37 @@
+const deepClone = inputObj => {
+  const valTypes = ['string', 'number', 'boolean', 'function', 'undefined'];
+  let clonedObj = {};
+
+  if (typeof inputObj !== 'object' || inputObj === null) {
+    return 'Input for deepClone() was not an object';
+  }
+
+  const cloneValue = val => {
+    let output = null;
+
+    if (val === null) {
+      return output;
+    } else if (valTypes.includes(typeof val)) {
+      output = val;
+    } else if (Array.isArray(val)) {
+      output = val.map( item => {
+        return cloneValue(item);
+      });
+    } else {
+      output = {};
+
+      for (let key in val) {
+        output[key] = cloneValue(val[key]);
+      }
+    }
+    return output;
+  };
+
+  for (let key in inputObj) {
+    clonedObj[key] = cloneValue(inputObj[key]);
+  }
+
+  return clonedObj;
+};
+
+console.log(deepClone([]))
